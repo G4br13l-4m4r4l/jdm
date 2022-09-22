d2c9d2388c1dba2b4f0c4136af9021137fcd4bb2
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2022-09-22 20:58:06 -0300

    init the project of the grid

---
+++ m-game/src/data/items.js
@@ -0,0 +1,8 @@
+export const items = [
+    {name: 'android', icon: 'android' },
+    {name: 'caminhao', icon: 'caminhao'},
+    {name: 'disney', icon: 'disney' },
+    {name: 'estrela', icon: 'estrela' },
+    {name: 'gasolina', icon: 'gasolina' },
+    {name: 'moto', icon: 'moto' }
+];
